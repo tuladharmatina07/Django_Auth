80380b887d111d9d418789b6bfbda71272f41d15
Matina Tuladhar <matinatuladhar@Matinas-MacBook-Pro.local>
2020-12-23 14:55:45 +0545

    Login Logout System

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as authview
 urlpatterns = [
-    path('', views.registration, name="Register"),
+    path('register/', views.registration, name="Register"),
+    path('login/', authview.LoginView.as_view(template_name="login.html"), name ="Login"),
+    path('logout/', authview.LogoutView.as_view(template_name="logout.html")),
+    path('', views.home, name="home"),
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegistrationForm
 from django.contrib import messages
 
 # Create your views here.
-
+def home(request):
+    return render(request,'home.html')
+    
 def registration(request):
     fm= RegistrationForm()
     if request.method == "POST":
@@ -12,9 +14,7 @@ def registration(request):
             fm.save()
             messages.success(request,
             'Registration Created Successfully')
+        return redirect('Login')
 
-        else:
-            fm = RegistrationForm()
-        
     return render(request, 'registration.html', {'fm': fm})
 
